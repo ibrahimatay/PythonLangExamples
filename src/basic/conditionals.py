8fc134848ddae3bf170ceced5b95700d37eee211
= <contact@ibrahimatay.com>
2020-08-21 11:48:04 +0300

    conditionals

---
+++ src/basic/conditionals.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+x = 1
+y = 3
+
+if x < y:
+    print('x ({}) is less than y ({})'.format(x, y))
+else:
+    print('a ({}) is not less than b ({})'.format(x, y))
