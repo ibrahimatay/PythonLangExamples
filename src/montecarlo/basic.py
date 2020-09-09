40c31fe5369588c8b527b970ee8a1177cca01401
İbrahim Atay <TR0039@turkre.local>
2020-09-09 11:36:55 +0300

    monte carlo - pi

---
+++ src/montecarlo/basic.py
@@ -0,0 +1,23 @@
+import random
+import math
+
+n_circle = 0
+n_square = 0
+
+for i in range(1_000_000):
+    x = random.random()
+    y = random.random()
+
+    if x**2 + y**2 <= 1:
+        n_circle += 1
+    
+    n_square += 1
+
+
+pi = 4.0 * n_circle / n_square
+
+
+
+print("approximate value of pi : {0}".format(pi))
+print("exact value of pi: {0}", math.pi)
+
