7793251442febfeec7853d760ac39648dd66d9af
Ibrahim ATAY <iatay@turkre.com>
2020-09-20 18:18:48 +0300

    parse type

---
+++ src/basic/parse-type.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+number = int('12') # 12
+print(number)
+
+text = str(number) # '12'
+print(text)
+
+logic = bool('') # False
+print(logic)
+
+logic = bool(None) # False
+print(logic)
+
+logic = bool('test') # True
+print(logic)
+
+number = int('1_000_000') # 1000000
+print(number)
+
+number = float('1000.093') # 1000.093
+print(number)
